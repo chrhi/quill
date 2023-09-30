474bf4e0e42d7723101da143ae06903dbf795032
abdellah <Abdellah.chehri14@gmail.com>
2023-09-30 14:21:18 +0200

    add

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,16 +1,14 @@
-'use client'
+"use client";
 
-import { trpc } from '@/app/_trpc/client'
-import { absoluteUrl } from '@/lib/utils'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
-import { PropsWithChildren, useState } from 'react'
+import { trpc } from "@/app/_trpc/client";
+import { absoluteUrl } from "@/lib/utils";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { PropsWithChildren, useState } from "react";
+import { Next13ProgressBar } from "next13-progressbar";
 
 const Providers = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
@@ -19,17 +17,21 @@ const Providers = ({ children }: PropsWithChildren) => {
         }),
       ],
     })
-  )
+  );
 
   return (
-    <trpc.Provider
-      client={trpcClient}
-      queryClient={queryClient}>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
+        <Next13ProgressBar
+          height="2px"
+          color="#0000ff"
+          options={{ showSpinner: false }}
+          showOnShallow
+        />
         {children}
       </QueryClientProvider>
     </trpc.Provider>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;
